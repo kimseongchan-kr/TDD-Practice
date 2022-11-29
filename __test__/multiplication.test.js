a86dca1875724c328edd2ad143468c93c43dc77b
kimseongchan-kr <fpel8800@gmail.com>
2022-11-29 18:51:58 +0900

    add: "같은 객체의 인스턴스인지 확인" 하는 함수의 테스트 코드 작성

--- __test__/multiplication.test.js
+++ __test__/multiplication.test.js
@@ -9,3 +9,7 @@ test('입력 달러 곱하기 환율', () => {
   product = five.times(3);
   expect(product.amount).toBe(15);
 });
+
+test('같은 객체의 인스턴스인지 확인', () => {
+  expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
+});
