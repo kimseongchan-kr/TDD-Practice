f2ef3edcdf68eb47d6c646a1a59e13e6980c256c
kimseongchan-kr <fpel8800@gmail.com>
2022-11-29 18:45:38 +0900

    mod: 수정된 테스트 코드에 맞춰 로직 변경 - 처음 작업 시 문법 상의 오류로 테스트가 실행이 안되니 return null로   임시 수정 - 위 작업을 진행 후 테스트가 진행이 되고 null이 리턴되니 10과 15에서   오류가 발생 - 로직을 테스트 코드에 맞춰 수정 진행

--- __test__/multiplication.test.js
+++ __test__/multiplication.test.js
@@ -2,10 +2,10 @@ import Dollar from '../src/Dollar';
 
 test('입력 달러 곱하기 환율', () => {
   const five = new Dollar(5);
-  let product = five.amount(2);
+  let product = five.times(2);
 
   expect(product.amount).toBe(10);
 
   product = five.times(3);
-  expect(five.amount).toBe(15);
+  expect(product.amount).toBe(15);
 });
