7320194e485b060ba9d624b07ca4f493d3e6d64b
kimseongchan-kr <fpel8800@gmail.com>
2022-12-05 20:25:41 +0900

    mod: 팩토리 메서드 방식으로 구조를 변경하기 전 테스트 코드 수정

--- __test__/multiplication.test.js
+++ __test__/multiplication.test.js
@@ -1,28 +1,26 @@
 import './assertEquals';
 
 import Money from '../src/Money';
-import Dollar from '../src/Dollar';
-import Franc from '../src/Franc';
 
 test('달러 곱셈 테스트', () => {
   const five = Money.dollar(5);
 
-  expect(true).assertEquals(new Dollar(10), five.times(2));
-  expect(true).assertEquals(new Dollar(15), five.times(3));
+  expect(true).assertEquals(Money.dollar(10), five.times(2));
+  expect(true).assertEquals(Money.dollar(15), five.times(3));
 });
 
 test('프랑 곱셈 테스트', () => {
-  const five = new Franc(5);
+  const five = Money.franc(5);
 
-  expect(true).assertEquals(new Franc(10), five.times(2));
-  expect(true).assertEquals(new Franc(15), five.times(3));
+  expect(true).assertEquals(Money.franc(10), five.times(2));
+  expect(true).assertEquals(Money.franc(15), five.times(3));
 });
 
 test('amount의 값과 인스턴스가 같은지 확인하는 테스트', () => {
-  expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
-  expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
-  expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
-  expect(new Franc(5).equals(new Franc(6))).toBeFalsy();
+  expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+  expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
+  expect(Money.franc(5).equals(Money.franc(5))).toBeTruthy();
+  expect(Money.franc(5).equals(Money.franc(6))).toBeFalsy();
 
-  expect(new Franc(5).equals(new Dollar(5))).toBeFalsy();
+  expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
 });
