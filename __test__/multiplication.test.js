fbe387ef88bb3ac5e643798da5dfd0ea61b4dd92
kimseongchan-kr <fpel8800@gmail.com>
2022-12-05 18:50:09 +0900

    mod: 객체에 직접적인 참조를 줄이기 위해 팩토리 패턴 사용

--- __test__/multiplication.test.js
+++ __test__/multiplication.test.js
@@ -1,10 +1,11 @@
 import './assertEquals';
 
+import Money from '../src/Money';
 import Dollar from '../src/Dollar';
 import Franc from '../src/Franc';
 
 test('달러 곱셈 테스트', () => {
-  const five = new Dollar(5);
+  const five = Money.dollar(5);
 
   expect(true).assertEquals(new Dollar(10), five.times(2));
   expect(true).assertEquals(new Dollar(15), five.times(3));
@@ -17,7 +18,7 @@ test('프랑 곱셈 테스트', () => {
   expect(true).assertEquals(new Franc(15), five.times(3));
 });
 
-test('같은 객체의 인스턴스인지 확인', () => {
+test('amount의 값과 인스턴스가 같은지 확인하는 테스트', () => {
   expect(new Dollar(5).equals(new Dollar(5))).toBeTruthy();
   expect(new Dollar(5).equals(new Dollar(6))).toBeFalsy();
   expect(new Franc(5).equals(new Franc(5))).toBeTruthy();
--- src/Money.js
+++ src/Money.js
@@ -1,15 +1,25 @@
 class Money {
-  amount;
+  #amount;
 
   constructor(amount) {
-    this.amount = amount;
+    this.#amount = amount;
+  }
+
+  get amount() {
+    return this.#amount;
   }
 
   equals(object) {
     const money = object;
 
-    return this.amount === money.amount && this.constructor.name === object.constructor.name;
+    return this.#amount === money.amount && this.constructor.name === object.constructor.name;
+  }
+
+  static dollar(amount) {
+    return new Dollar(amount);
   }
 }
 
 export default Money;
+
+class Dollar {}
