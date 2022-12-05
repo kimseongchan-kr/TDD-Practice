b6cbde3e3cd053df3f6ac25d42a8c38831b9aade
kimseongchan-kr <fpel8800@gmail.com>
2022-12-05 18:18:00 +0900

    mod: Franc 객체, 불필요한 객체 제거

--- src/Franc.js
+++ src/Franc.js
@@ -1,10 +1,6 @@
 import Money from './Money';
 
 class Franc extends Money {
-  get amount() {
-    return this.amount;
-  }
-
   times(multiplier) {
     return new Franc(this.amount * multiplier);
   }
