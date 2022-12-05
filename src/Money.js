9185cc35fcd36975fcdb57498955985424b526b7
kimseongchan-kr <fpel8800@gmail.com>
2022-12-05 20:26:13 +0900

    mod: 펙토리 메서드 방식으로 구조 변경

--- src/Money.js
+++ src/Money.js
@@ -3,6 +3,9 @@ class Money {
 
   constructor(amount) {
     this.#amount = amount;
+    if (this.times === undefined) {
+      throw new TypeError('Must override method');
+    }
   }
 
   get amount() {
@@ -18,8 +21,30 @@ class Money {
   static dollar(amount) {
     return new Dollar(amount);
   }
+
+  static franc(amount) {
+    return new Franc(amount);
+  }
+}
+
+class Dollar extends Money {
+  constructor(amount) {
+    super(amount);
+  }
+
+  times(multiplier) {
+    return new Dollar(super.amount * multiplier);
+  }
+}
+
+class Franc extends Money {
+  constructor(amount) {
+    super(amount);
+  }
+
+  times(multiplier) {
+    return new Franc(super.amount * multiplier);
+  }
 }
 
 export default Money;
-
-class Dollar {}
