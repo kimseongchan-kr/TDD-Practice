d0fd3a35e983c3a662268c2743a393cd8111803e
kimseongchan-kr <fpel8800@gmail.com>
2022-12-10 19:11:17 +0900

    mod: 프랑 곱셉 테스트 제거

--- __test__/multiplication.test.js
+++ __test__/multiplication.test.js
@@ -9,13 +9,6 @@ test('달러 곱셈 테스트', () => {
   expect(true).assertEquals(Money.dollar(15), five.times(3));
 });
 
-test('프랑 곱셈 테스트', () => {
-  const five = Money.franc(5);
-
-  expect(true).assertEquals(Money.franc(10), five.times(2));
-  expect(true).assertEquals(Money.franc(15), five.times(3));
-});
-
 test('amount의 값과 인스턴스가 같은지 확인하는 테스트', () => {
   expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
   expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
