9073dc0c2938679ee1fd1dc8fd6aa9eb0937f247
kimseongchan-kr <fpel8800@gmail.com>
2022-12-12 18:55:34 +0900

    test(money): 파일 명 변경, 테스트 코드 함수명 이름 변경

---
+++ __test__/money.test.js
@@ -0,0 +1,30 @@
+import './assertEquals';
+
+import Money from '../src/Money';
+
+test('화폐 곱셈 테스트', () => {
+  const five = Money.dollar(5);
+
+  expect(true).assertEquals(Money.dollar(10), five.times(2));
+  expect(true).assertEquals(Money.dollar(15), five.times(3));
+});
+
+test('달러의 합 테스트', () => {
+  const sum = Money.dollar(5).plus(Money.dollar(5));
+
+  expect(true).assertEquals(Money.dollar(10), sum);
+});
+
+test('다중 화폐의 합 테스트', () => {});
+
+test('amount의 값과 인스턴스가 같은지 확인하는 테스트', () => {
+  expect(Money.dollar(5).equals(Money.dollar(5))).toBeTruthy();
+  expect(Money.dollar(5).equals(Money.dollar(6))).toBeFalsy();
+
+  expect(Money.franc(5).equals(Money.dollar(5))).toBeFalsy();
+});
+
+test('인스턴스에 맞는 통화 문자열을 가지고 있는지 확인하는 테스트', () => {
+  expect(Money.dollar(1).currency).toBe('USD');
+  expect(Money.franc(1).currency).toBe('CHF');
+});
