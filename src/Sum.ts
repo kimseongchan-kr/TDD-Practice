cf18e6aa29da1ad0437c58e0efbfb3aaf6ee9a7b
kimseongchan-kr <fpel8800@gmail.com>
2023-01-02 19:54:47 +0900

    mod(Sum): reduce()가 축약(환율)될 수 있도록 수정

--- src/Sum.ts
+++ src/Sum.ts
@@ -12,7 +12,7 @@ class Sum implements Expression {
   }
 
   reduce(bank: Bank, to: string) {
-    const amount = this.augend.amount + this.addend.amount;
+    const amount = this.augend.reduce(bank, to).amount + this.addend.reduce(bank, to).amount;
     return new Money(amount, to);
   }
 }
